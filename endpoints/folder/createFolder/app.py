40b424d6e7df6bd6e82d7ff6173190e2710a86d2
JJerome-NM <mykhailo.khalabarchuk.21@pnu.edu.ua>
2025-03-02 18:24:10 +0200

    Added createFolder and getFolder endpoints business logic

--- endpoints/folder/createFolder/app.py
+++ endpoints/folder/createFolder/app.py
@@ -1,19 +1,73 @@
 import json
+import base64
+import re
+import uuid
+
 import boto3
 
 TABLE_NAME = "folder_table"
+UUID_REGEX = re.compile(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', re.IGNORECASE)
+
+person_table = boto3.resource('dynamodb', endpoint_url="http://host.docker.internal:8000").Table(TABLE_NAME)
 
 def lambda_handler(event, context):
-    print("---- Start local logs --------------------------------------")
-    # print(event)
+    if "body" not in event or not event["body"]:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'No body provided'})
+        }
 
-    person_table = (boto3
-                    .resource('dynamodb', endpoint_url="http://host.docker.internal:8000") # TODO Change this
-                    .Table(TABLE_NAME))
+    try:
+        body_decoded = base64.b64decode(event['body']).decode('utf-8')
+        data = json.loads(body_decoded)
+    except Exception as e:
+        print("Body decode error:", e)
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Invalid body format'})
+        }
 
-    list_items = person_table.scan()
+    if "folderName" not in data or not isinstance(data["folderName"], str):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'folderName must be provided and be a string'})
+        }
+
+    if "parentFolderId" not in data or not isinstance(data["parentFolderId"], str):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'parentFolderId must be provided and be a string'})
+        }
+
+    if not UUID_REGEX.match(data["parentFolderId"]):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'parentFolderId is not a valid UUID'})
+        }
+
+    print("Valid request data:", data)
+
+    claims = event.get("requestContext", {}).get("authorizer", {}).get("claims", {})
+    user_id = claims.get("sub") or "local-test-user-id" # TODO FIX THIS
+
+    if not user_id:
+        return {'statusCode': 403, 'body': 'User is unauthorized'}
+
+
+    folder_id = str(uuid.uuid4())
+
+    person_table.put_item(
+        Item={
+            "folderId": folder_id,
+            "ownerId": user_id,
+            "folderName": data["folderName"],
+            "parentFolderId": data["parentFolderId"],
+        }
+    )
+
+    new_folder = person_table.get_item(Key={"folderId": folder_id})
 
     return {
         "statusCode": 200,
-        "body": json.dumps(list_items['Items']),
+        "body": json.dumps(new_folder["Item"])
     }
--- endpoints/folder/getFolder/app.py
+++ endpoints/folder/getFolder/app.py
@@ -1,20 +1,43 @@
 import json
-import boto3
+import re
 
-TABLE_NAME = "folder_table"
+import boto3
+from boto3.dynamodb.conditions import Key
+
+FOLDERS_TABLE_NAME = "folder_table"
+PHOTOS_TABLE_NAME = "photo_table"
+UUID_REGEX = re.compile(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', re.IGNORECASE)
+
+FOLDERS_TABLE = boto3.resource('dynamodb', endpoint_url="http://host.docker.internal:8000").Table(FOLDERS_TABLE_NAME)
+PHOTOS_TABLE = boto3.resource('dynamodb', endpoint_url="http://host.docker.internal:8000").Table(PHOTOS_TABLE_NAME)
+
 
 def lambda_handler(event, context):
-    print("---- Start local logs --------------------------------------")
     folder_id = event.get('pathParameters', {}).get('id')
     print("Folder ID:", folder_id)
 
-    person_table = (boto3
-                    .resource('dynamodb', endpoint_url="http://host.docker.internal:8000") # TODO Change this
-                    .Table(TABLE_NAME))
+    if not UUID_REGEX.match(folder_id):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Folder if is not a valid UUID'})
+        }
 
-    list_items = person_table.scan()
+    existing_folder = FOLDERS_TABLE.get_item(Key={"folderId": folder_id})
+    photos_in_folder = PHOTOS_TABLE.query(
+        IndexName="folderId-index",
+        KeyConditionExpression=Key("folderId").eq(folder_id)
+    )
+
+    if "Item" not in existing_folder:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({'error': f"item with id {folder_id} not found"})
+        }
 
     return {
         "statusCode": 200,
-        "body": json.dumps(list_items['Items']),
+        "body": json.dumps({
+            "folder": existing_folder['Item'],
+            "photos": photos_in_folder['Items']
+        }),
     }
